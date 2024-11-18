81085c818b3147fb2248a18c1d8f81d2625e5c2e
Student <student@users-MacBook-Pro.local>
2024-11-18 21:12:50 +0600

    firebase configure

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,45 @@
+import 'package:dynamic_ui/firebase_options.dart';
+import 'package:dynamic_ui/screens/first_tab_screen.dart';
+import 'package:dynamic_ui/screens/second_tab_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dynamic UI',
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(text: 'Dynamic Screen'),
+                Tab(text: 'Tasks'),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              FirstTabScreen(),
+              SecondTabScreen(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 
---
+++ lib/screens/second_tab_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SecondTabScreen extends StatelessWidget {
+  const SecondTabScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
